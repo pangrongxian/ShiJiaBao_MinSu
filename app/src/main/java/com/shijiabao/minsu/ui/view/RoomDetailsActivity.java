7ebb09a7e640c11780018fe1ca342be6a1cd3e9d
Rongxian <859358014@qq.com>
2016-04-22 15:18:12 +0800

    添加了房间详情页

---
+++ app/src/main/java/com/shijiabao/minsu/ui/view/RoomDetailsActivity.java
@@ -0,0 +1,15 @@
+package com.shijiabao.minsu.ui.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shijiabao.minsu.R;
+
+public class RoomDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_details);
+    }
+}
