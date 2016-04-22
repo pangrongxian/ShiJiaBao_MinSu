a77dd9cd3eb931a9ef7ddeec644d11105c741fac
Rongxian <859358014@qq.com>
2016-04-22 15:55:26 +0800

    增加了订单详情页，还未完成。。

---
+++ app/src/main/java/com/shijiabao/minsu/ui/view/OrderDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.shijiabao.minsu.ui.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shijiabao.minsu.R;
+
+public class OrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_details);
+
+    }
+}
