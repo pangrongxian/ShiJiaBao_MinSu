ab44152496bd74929f39bd9d6a66c51be2b3ffd6
Rongxian <859358014@qq.com>
2016-04-27 09:11:03 +0800

    不知道到底是不是混淆文件的原因，ButterrKnife点击事件失效，所以改成普通的寻找控件和设置监听事件。

--- app/src/main/java/com/shijiabao/minsu/ui/view/AppointmentActivity.java
+++ app/src/main/java/com/shijiabao/minsu/ui/view/AppointmentActivity.java
@@ -3,30 +3,27 @@ package com.shijiabao.minsu.ui.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+import android.view.View;
 
 import com.shijiabao.minsu.R;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
 public class AppointmentActivity extends AppCompatActivity {
 
-    @Bind(R.id.commitBtn)
-    Button commitBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
-        ButterKnife.bind(this);
+
     }
 
-    @OnClick(R.id.commitBtn)
-    public void onClick() {
+    public void commit(View view) {
         //点击跳转到订单详情界面
         Intent intent = new Intent(this, OrderDetailsActivity.class);
         startActivity(intent);
     }
+
+
+
 }
